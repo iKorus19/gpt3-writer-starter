6fbc0b4fb42ef51a8a62c81729f31348ab57c64f
iKorus19 <55372681+iKorus19@users.noreply.github.com>
2022-12-29 16:19:59 -0800

    latest build

--- pages/_document.js
+++ pages/_document.js
@@ -4,11 +4,11 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
+        <meta property="og:title" content="Blog it up, Bitch!" key="title"/>
+        <meta property="og:description" content="Ai Blog Writing Assistant" key="description"/>
         <meta
           property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+          content="http://thejonathonjames.com/wp-content/uploads/2022/12/share-image.png"
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
